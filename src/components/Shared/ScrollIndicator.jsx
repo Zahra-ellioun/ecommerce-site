7f46379e38b1203872831d000e63711a548e41a6
zahra ellioun <zizi.ell75@gmail.com>
2025-08-21 16:00:17 +0330

    write formola for scroll value

--- src/components/Shared/ScrollIndicator.jsx
+++ src/components/Shared/ScrollIndicator.jsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+
 const ScrollIndicator = () => {
-  return <div className=" w-full h-2 bg-gray-400"></div>;
+  const [scrollWidth, setScrollWidth] = useState(0);
+
+  const scrollTopValue = document.documentElement.scrollTop;
+  const scrollHeightValue = document.documentElement.scrollHeight;
+  const ClientHeightValue = document.documentElement.clientHeight;
+
+  const scrollValue =
+    (scrollTopValue / (scrollHeightValue - ClientHeightValue)) * 100;
+
+  useEffect(() => {
+    setScrollWidth(scrollValue);
+  }, [scrollValue]);
+
+  return (
+    <div className=" w-full h-2 bg-gray-400">
+      <div
+        className="h-full bg-red-500"
+        style={{ width: `${scrollWidth}%` }}
+      ></div>
+    </div>
+  );
 };
 
 export default ScrollIndicator;

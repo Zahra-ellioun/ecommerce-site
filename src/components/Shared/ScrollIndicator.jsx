a25589a9352c71b3927373a5a8e8a30967991b69
zahra ellioun <zizi.ell75@gmail.com>
2025-08-21 16:54:46 +0330

    add useeffect for eventlistener scroll

--- src/components/Shared/ScrollIndicator.jsx
+++ src/components/Shared/ScrollIndicator.jsx
@@ -3,16 +3,29 @@ import { useEffect, useState } from "react";
 const ScrollIndicator = () => {
   const [scrollWidth, setScrollWidth] = useState(0);
 
-  const scrollTopValue = document.documentElement.scrollTop;
-  const scrollHeightValue = document.documentElement.scrollHeight;
-  const ClientHeightValue = document.documentElement.clientHeight;
-
-  const scrollValue =
-    (scrollTopValue / (scrollHeightValue - ClientHeightValue)) * 100;
-
+  //   از یوز افکت استفاده می کنیم چون باید وقتی کامپوننت لود شد، گوش دادن به اسکرول شروع بشه و وقتی کامپوننت از بین رفت، گوش دادن قطع بشه.
   useEffect(() => {
-    setScrollWidth(scrollValue);
-  }, [scrollValue]);
+    const handleScrollIndicator = () => {
+      // بدست اوردن مقادیر در زمان اسکرول
+      const scrollTopValue = document.documentElement.scrollTop;
+      const scrollHeightValue = document.documentElement.scrollHeight;
+      const ClientHeightValue = document.documentElement.clientHeight;
+      //   بدست اوردن درصد اسکرول شده
+      const scrollValue = Math.round(
+        (scrollTopValue / (scrollHeightValue - ClientHeightValue)) * 100
+      );
+
+      setScrollWidth(scrollValue);
+      console.log("مقدار اسکرول", scrollValue);
+    };
+    // در زمان اسکرول شدن فانکشن اجرا بشه
+    window.addEventListener("scroll", handleScrollIndicator);
+
+    // پاک شدن ایونت لیستنر برای مصرف نشدن حافظه اضافه بعد از قطع اسکرول
+    return () => {
+      window.removeEventListener("scroll", handleScrollIndicator);
+    };
+  }, []);
 
   return (
     <div className=" w-full h-2 bg-gray-400">
